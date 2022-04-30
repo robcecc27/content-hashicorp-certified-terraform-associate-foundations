9714b011983417f2a36c7cfe8f79221eb6b19578
robcecc27 <rob.cecchini@gmail.com>
2022-04-30 15:37:37 +0300

    added ubuntu

--- section4-lesson2/main.tf
+++ section4-lesson2/main.tf
@@ -6,11 +6,24 @@ resource "docker_image" "terraform-centos" {
   name         = "centos:7"
   keep_locally = true
 }
+# Pulls the image
+resource "docker_image" "terraform-ubuntu" {
+  name         = "ubuntu"
+  keep_locally = true
+}
 
 # Create a container
 resource "docker_container" "centos" {
   image   = docker_image.terraform-centos.latest
   name    = "terraform-centos"
+  start   = true
+  command = ["/bin/sleep", "500"]
+}
+
+# Create a container
+resource "docker_container" "ubuntu" {
+  image   = docker_image.terraform-ubuntu.latest
+  name    = "terraform-ubuntu"
   start   = true
   command = ["/bin/sleep", "500"]
 }
